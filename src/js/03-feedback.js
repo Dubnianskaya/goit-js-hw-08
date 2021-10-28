b294e8567a9a41212db38cf0b00e9dba7fe05944
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2021-10-28 21:54:44 +0300

    fixed 3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,23 +4,27 @@ const FEEDBACK_FORM_KEY = "feedback-form-state"
 form.addEventListener("input", throttle(onSavingFormData, 500));
 form.addEventListener("submit", onFormSubmit)
 filledForm()
-const feedbackFormData = {}
 function onSavingFormData(event) {
+    let feedbackFormData = localStorage.getItem(FEEDBACK_FORM_KEY);
+    feedbackFormData = feedbackFormData ? JSON.parse(feedbackFormData) : {};
     feedbackFormData[event.target.name] = event.target.value
     localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(feedbackFormData))
 }
 function onFormSubmit(event) {
+    console.log(JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY)))
     event.preventDefault();
     event.currentTarget.reset();
-    localStorage.removeItem(FEEDBACK_FORM_KEY)
-    console.log(feedbackFormData)
+    localStorage.removeItem(FEEDBACK_FORM_KEY);
 }
 
 function filledForm() {
-    const savedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+    let savedData = localStorage.getItem(FEEDBACK_FORM_KEY);
     if(savedData) {
-            form.email.value = savedData.email;
-            form.message.value = savedData.message;
+        savedData = JSON.parse(savedData);
+        Object.entries(savedData).forEach(([name, value]) => {
+            form.elements[name].value = value;
+        });
     }
+    
 }
 
